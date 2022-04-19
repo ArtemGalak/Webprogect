b15c491c03a12dc9007ba19713414f595ae3bdf6
Slash <ew.kolesnikov1969@yandex.ru>
2022-04-20 00:53:54 +0500

    Добавил регистрацию и авторизацию пользователей, улучшил заказ и список заказов

--- Baseworker.py
+++ Baseworker.py
@@ -2,10 +2,9 @@ import csv
 import os
 import sqlite3
 
-from flask import Flask, render_template, request, g, flash, redirect
+from flask import Flask, render_template, request, g, redirect
 from flask_login import LoginManager
 from flask_login import login_user, current_user
-from werkzeug.security import generate_password_hash
 
 from HelpBase import HelpBase
 from data import db_session
@@ -13,8 +12,11 @@ from data.admins import Admin
 from data.calls import Calls
 from data.goods import Goods
 from data.orders import Orders
+from data.users import Users
 from forms.admin import AdminLoginForm
 from forms.good import GoodForm
+from forms.user import UserLoginForm
+from forms.user_reg import UserRegForm
 
 DATABASE = 'Base.db'
 SECRET_KEY = 'dfbfdbd;.fbdf><dfbdf&3435!@3l'
@@ -45,6 +47,8 @@ dbase = None
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
+    if int(user_id) > 0:
+        return db_sess.query(Users).get(user_id)
     return db_sess.query(Admin).get(user_id)
 
 
@@ -62,22 +66,41 @@ def main():
 
 
 @app.route('/Авторизация.html', methods=['GET', 'POST'])
-def reqister():
-    if request.method == "POST":
-        if len(request.form['name']) > 3 and '@' in request.form['email'] \
-                and len(request.form['text']):
-            hash = generate_password_hash((request.form['text']))
-            res = dbase.addUser(request.form['name'], request.form['email'], hash)
-            print(res)
-            if res:
-                flash("Вы успешно зарегестрировались!", "seccess")
-                return redirect('/Главная.html')
-    return render_template('/page_1/Авторизация.html', title='Регистрация')
+def login():
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(Users).filter(Users.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            return redirect("/")
+        return render_template('page_1/Авторизация.html', form=form,
+                               message="Неправильный логин или пароль")
+    print(form.errors)
+    return render_template('page_1/Авторизация.html', form=form)
 
 
-@app.route('/Регистрация.html')
+@app.route('/Регистрация.html', methods=['GET', 'POST'])
 def register():
-    return render_template('page_1/Регистрация.html', title='О-нас')
+    form = UserRegForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        users = db_sess.query(Users).filter(Users.email == form.email.data).first()
+        if users:
+            return render_template('page_1/Регистрация.html',
+                                   message="Пользователь с такой почтой уже существует",
+                                   form=form)
+        else:
+            user = Users()
+            user.name = form.name.data
+            user.email = form.email.data
+            user.password = user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            use = db_sess.query(Users).filter(Users.email == form.email.data).first()
+            login_user(use)
+            return redirect("/")
+    return render_template('page_1/Регистрация.html', form=form)
 
 
 @app.route('/О-нас.html')
@@ -107,56 +130,60 @@ def contact():
 
 @app.route('/order', methods=['GET', 'POST'])
 def order():
-    form = GoodForm()
-    db_sess = db_session.create_session()
-    goodses = db_sess.query(Goods).all()
-    if request.method == 'POST':
-        if request.form.get('Update') == 'Обновить корзину':
-            zakaz = {}
-            control_sum = 0
-            for elem in goodses:
-                zakaz[elem.brend + elem.title] = int(request.form.get(elem.brend + elem.title))
-                control_sum += int(request.form.get(elem.brend + elem.title)) * elem.price
-            return render_template('page_1/Заказ.html', form=form, goodses=goodses, zakaz=zakaz,
-                                   control_sum=control_sum)
-        elif request.form.get('Update') == 'Заказать':
-            orders_spis = db_sess.query(Orders).all()
-            orders = Orders()
-            orders.href = f"static/csv/{len(orders_spis)}.csv"
-            orders.email = 'email'
-            orders.is_done = False
-            db_sess.add(orders)
-            db_sess.commit()
-            with open(orders.href, mode="w", encoding='cp1251', newline='') as f:
-                db_sess = db_session.create_session()
-                goods = db_sess.query(Goods).all()
-                f.truncate()
-                pricewriter = csv.writer(f, delimiter=';')
-                pricewriter.writerow(['id', 'Бренд', 'Название', 'Количество', 'Цена', 'Итог'])
+    if current_user.is_authenticated:
+        form = GoodForm()
+        db_sess = db_session.create_session()
+        goodses = db_sess.query(Goods).all()
+        if request.method == 'POST':
+            if request.form.get('Update') == 'Обновить корзину':
+                zakaz = {}
                 control_sum = 0
-                for good in goodses:
-                    pricewriter.writerow([good.id, good.brend, good.title,
-                                          int(request.form.get(good.brend + good.title)), good.price,
-                                          int(request.form.get(good.brend + good.title)) * good.price])
-                    control_sum += int(request.form.get(good.brend + good.title)) * good.price
-                pricewriter.writerow(['', '', '', '', '', str(control_sum)])
-            f.close()
+                for elem in goodses:
+                    zakaz[elem.brend + elem.title] = int(request.form.get(elem.brend + elem.title))
+                    control_sum += int(request.form.get(elem.brend + elem.title)) * elem.price
+                return render_template('page_1/Заказ.html', form=form, goodses=goodses, zakaz=zakaz,
+                                       control_sum=control_sum)
+            elif request.form.get('Update') == 'Заказать':
+                orders_spis = db_sess.query(Orders).all()
+                orders = Orders()
+                orders.href = f"static/csv/{len(orders_spis)}.csv"
+                db_sess = db_session.create_session()
+                user = db_sess.query(Users).filter(Users.id == current_user.get_id()).first()
+                orders.email = user.email
+                orders.is_done = False
+                db_sess.add(orders)
+                db_sess.commit()
+                with open(orders.href, mode="w", encoding='cp1251', newline='') as f:
+                    db_sess = db_session.create_session()
+                    goods = db_sess.query(Goods).all()
+                    f.truncate()
+                    pricewriter = csv.writer(f, delimiter=';')
+                    pricewriter.writerow(['id', 'Бренд', 'Название', 'Количество', 'Цена', 'Итог'])
+                    control_sum = 0
+                    for good in goodses:
+                        pricewriter.writerow([good.id, good.brend, good.title,
+                                              int(request.form.get(good.brend + good.title)), good.price,
+                                              int(request.form.get(good.brend + good.title)) * good.price])
+                        control_sum += int(request.form.get(good.brend + good.title)) * good.price
+                    pricewriter.writerow(['', '', '', '', '', str(control_sum)])
+                f.close()
+                zakaz = {}
+                control_sum = 0
+                for elem in goodses:
+                    zakaz[elem.brend + elem.title] = int(request.form.get(elem.brend + elem.title))
+                    control_sum += int(request.form.get(elem.brend + elem.title)) * elem.price
+                return render_template('page_1/Заказ.html', form=form, goodses=goodses, zakaz=zakaz,
+                                       control_sum=control_sum,
+                                       thank_you=f'Спасибо за заказ, ваша заявка №{len(orders_spis)}'
+                                                 f' уже находится в обработке. На вашу почту придёт письмо')
+            else:
+                return render_template('page_1/Заказ.html', form=form, goodses=goodses)
+        elif request.method == 'GET':
             zakaz = {}
-            control_sum = 0
             for elem in goodses:
-                zakaz[elem.brend + elem.title] = int(request.form.get(elem.brend + elem.title))
-                control_sum += int(request.form.get(elem.brend + elem.title)) * elem.price
-            return render_template('page_1/Заказ.html', form=form, goodses=goodses, zakaz=zakaz,
-                                   control_sum=control_sum,
-                                   thank_you=f'Спасибо за заказ, ваша заявка №{len(orders_spis)}'
-                                             f' уже находится в обработке. На вашу почту придёт письмо')
-        else:
-            return render_template('page_1/Заказ.html', form=form, goodses=goodses)
-    elif request.method == 'GET':
-        zakaz = {}
-        for elem in goodses:
-            zakaz[elem.brend + elem.title] = 0
-        return render_template('page_1/Заказ.html', form=form, goodses=goodses, zakaz=zakaz, control_sum=0)
+                zakaz[elem.brend + elem.title] = 0
+            return render_template('page_1/Заказ.html', form=form, goodses=goodses, zakaz=zakaz, control_sum=0)
+    return redirect('/Авторизация.html')
 
 
 @app.route('/price_list')
@@ -174,7 +201,7 @@ def price_list():
 
 
 @app.route('/login_admin', methods=['GET', 'POST'])
-def login():
+def login_admin():
     form = AdminLoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
@@ -191,7 +218,7 @@ def login():
 @app.route('/add_good_new_brend', methods=['GET', 'POST'])
 def add_good_new_brand():
     if current_user.is_authenticated:
-        if current_user.get_id() == '1':
+        if current_user.get_id() == '0':
             form = GoodForm()
             if form.validate_on_submit():
                 db_sess = db_session.create_session()
@@ -211,7 +238,7 @@ def add_good_new_brand():
 @app.route('/add_good_old_brend', methods=['GET', 'POST'])
 def add_good_old_brand():
     if current_user.is_authenticated:
-        if current_user.get_id() == '1':
+        if current_user.get_id() == '0':
             form = GoodForm()
             db_sess = db_session.create_session()
             brends = db_sess.query(Goods.brend).distinct()
@@ -241,7 +268,7 @@ def add_good_old_brand():
 @app.route('/change_product', methods=['GET', 'POST'])
 def change_product():
     if current_user.is_authenticated:
-        if current_user.get_id() == '1':
+        if current_user.get_id() == '0':
             form = GoodForm()
             db_sess = db_session.create_session()
             brends = db_sess.query(Goods.brend).distinct()
@@ -267,7 +294,7 @@ def change_product():
 @app.route('/order_requests', methods=['GET', 'POST'])
 def order_requests():
     if current_user.is_authenticated:
-        if current_user.get_id() == '1':
+        if current_user.get_id() == '0':
             form = AdminLoginForm()
             db_sess = db_session.create_session()
             order_request = db_sess.query(Orders)
@@ -278,7 +305,12 @@ def order_requests():
                 db_sess.commit()
                 return render_template('site_admin/Заявки-на-заказ.html', form=form, order_request=order_request)
             if request.method == 'GET':
-                return render_template('site_admin/Заявки-на-заказ.html', form=form, order_request=order_request)
+                names = db_sess.query(Users).all()
+                name = {}
+                for elem in names:
+                    name[elem.email] = elem.name
+                return render_template('site_admin/Заявки-на-заказ.html', form=form,
+                                       order_request=order_request, name=name)
         return redirect('/')
     return redirect('/login_admin')
 
@@ -286,7 +318,7 @@ def order_requests():
 @app.route('/call_requests', methods=['GET', 'POST'])
 def call_requests():
     if current_user.is_authenticated:
-        if current_user.get_id() == '1':
+        if current_user.get_id() == '0':
             form = AdminLoginForm()
             db_sess = db_session.create_session()
             call_request = db_sess.query(Calls)
--- data/__all_models.py
+++ data/__all_models.py
@@ -2,3 +2,4 @@ from . import goods
 from . import admins
 from . import orders
 from . import calls
+from . import users
